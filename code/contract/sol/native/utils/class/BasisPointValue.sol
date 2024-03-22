700ee63acd0354e63ef1f0f57f103d7dee97b3e3
snqre <marco.bizxaro@gmail.com>
2024-03-22 00:48:27 +0000

    =>

--- code/contract/sol/native/utils/class/BasisPointValue.sol
+++ code/contract/sol/native/utils/class/BasisPointValue.sol
@@ -2,6 +2,6 @@
 pragma solidity >=0.8.19;
 import { FixedPointValue } from "./FixedPointValue.sol";
 
-struct BasisPoint {
+struct BasisPointValue {
     FixedPointValue value;
 }
--- code/contract/sol/native/utils/math/FixedPointMath.sol
+++ code/contract/sol/native/utils/math/FixedPointMath.sol
@@ -11,110 +11,127 @@ library FixedPointMath {
 
     error IncompatibleDecimals(uint8 decimals0, uint8 decimals1);
 
-    function add(FixedPointValue memory num0, FixedPointValue memory num1) public pure returns (FixedPointValue memory) {
-        uint8 decimals0;
-        uint8 decimals1;
+    function scaleOf(FixedPointValue memory num0, FixedPointValue memory num1) external pure returns (BasisPointValue memory) {
+        return scaleOf_(num0, num1);
+    }
+
+    function sliceOf(FixedPointValue memory num, BasisPointValue memory slice) external pure returns (FixedPointValue memory) {
+        return sliceOf_(num, slice);
+    }
+
+    function addedValue(FixedPointValue memory num0, FixedPointValue memory num1) external pure returns (FixedPointValue memory) {
+        return add_(num0, num1);
+    }
+
+    function subedValue(FixedPointValue memory num0, FixedPointValue memory num1) external pure returns (FixedPointValue memory) {
+        return sub_(num0, num1);
+    }
+
+    function muledValue(FixedPointValue memory num0, FixedPointValue memory num1) external pure returns (FixedPointValue memory) {
+        return mul_(num0, num1);
+    }
+
+    function divedValue(FixedPointValue memory num0, FixedPointValue memory num1) external pure returns (FixedPointValue memory) {
+        return div_(num0, num1);
+    }
+
+    function valueWithNewDecimals(FixedPointValue memory num, uint8 decimals) external pure returns (FixedPointValue memory) {
+        return valueWithNewDecimals_(num, decimals);
+    }
+
+    function valueWithEtherDecimals(FixedPointValue memory num) external pure returns (FixedPointValue memory) {
+        return valueWithEtherDecimals_(num);
+    }
+
+    function scaleOf_(FixedPointValue memory num0, FixedPointValue memory num1) private pure returns (BasisPointValue memory) {
+        requireCompatibleDecimals_(num0, num1);
+        uint8 decimals;
+        uint256 representation;
+        FixedPointValue memory result;
+        FixedPointValue memory scale;
+        decimals = num0.decimals;
+        value0 = num0.value;
+        value1 = num1.value;
+        representation = 10**decimals;
+        scale = FixedPointValue({value: 10_000 * representation, decimals: decimals});
+        result = num0.divedValue_(num1).muledValue_(scale);
+        return BasisPointValue({value: result});
+    }
+
+    function sliceOf_(FixedPointValue memory num, BasisPointValue memory slice) private pure returns (FixedPointValue memory) {
+        requireCompatibleDecimals_(num, portion.value.decimals);
+        uint8 decimals;
+        uint256 representation;
+        FixedPointValue memory result;
+        FixedPointValue memory scale;
+        decimals = num.decimals;
+        representation = 10**decimals;
+        scale = FixedPointValue({value: 10_000 * representation}, decimals: decimals);
+        result = num.divedValue_(scale).muledValue_(slice);
+        return result;
+    }
+
+    function addedValue_(FixedPointValue memory num0, FixedPointValue memory num1) private pure returns (FixedPointValue memory) {
+        requireCompatibleDecimals_(num0, num1);
+        uint8 decimals;
         uint256 value0;
         uint256 value1;
         uint256 result;
-        decimals0 = num0.decimals;
-        decimals1 = num1.decimals;
+        decimals = num0.decimals;
         value0 = num0.value;
         value1 = num1.value;
         result = value0 + value1;
-        if (decimals0 != decimals1) revert IncompatibleDecimals(decimals0, decimals1);
-        return FixedPointValue({value: result, decimals: decimals0});        
+        return FixedPointValue({value: result, decimals: decimals});
     }
 
-    function sub(FixedPointValue memory num0, FixedPointValue memory num1) public pure returns (FixedPointValue memory) {
-        uint8 decimals0;
-        uint8 decimals1;
+    function subedValue_(FixedPointValue memory num0, FixedPointValue memory num1) private pure returns (FixedPointValue memory) {
+        requireCompatibleDecimals_(num0, num1);
+        uint8 decimals;
         uint256 value0;
         uint256 value1;
         uint256 result;
-        decimals0 = num0.decimals;
-        decimals1 = num1.decimals;
+        decimals = num0.decimals;
         value0 = num0.value;
         value1 = num1.value;
         result = value0 - value1;
-        if (decimals0 != decimals1) revert IncompatibleDecimals(decimals0, decimals1);
-        return FixedPointValue({value: result, decimals: decimals0});
+        return FixedPointValue({value: result, decimals: decimals});
     }
 
-    function mul(FixedPointValue memory num0, FixedPointValue memory num1) public pure returns (FixedPointValue memory) {
-        uint8 decimals0;
-        uint8 decimals1;
+    function muledValue_(FixedPointValue memory num0, FixedPointValue memory num1) private pure returns (FixedPointValue memory) {
+        requireCompatibleDecimals_(num0, num1);
+        uint8 decimals;
         uint256 value0;
         uint256 value1;
         uint256 result;
         uint256 representation;
-        decimals0 = num0.decimals;
-        decimals1 = num1.decimals;
+        decimals = num0.decimals;
         value0 = num0.value;
         value1 = num1.value;
-        if (decimals0 != decimals1) revert IncompatibleDecimals(decimals0, decimals1);
+        representation = 10**decimals;
         if (decimals0 == 0) {
             result = value0 * value1;
-            return FixedPointValue({value: result, decimals: decimals0});
+            return FixedPointValue({value: result, decimals: decimals});
         }
-        if (value0 == 0 || value1 == 0) return FixedPointValue({value: 0, decimals: decimals0});
-        representation = 10**decimals0;
         result = value0.mulDiv(value1, representation);
-        return FixedPointValue({value: result, decimals: decimals0});
+        return FixedPointValue({value: result, decimals: decimals});
     }
 
-    function div(FixedPointValue memory num0, FixedPointValue memory num1) public pure returns (FixedPointValue memory) {
-        uint8 decimals0;
-        uint8 decimals1;
+    function divedValue_(FixedPointValue memory num0, FixedPointValue memory num1) private pure returns (FixedPointValue memory) {
+        requireCompatibleDecimals_(num0, num1);
+        uint8 decimals;
         uint256 value0;
         uint256 value1;
         uint256 result;
         uint256 representation;
-        decimals9 = num0.decimals;
-        decimals1 = num1.decimals;
+        decimals = num0.decimals;
         value0 = num0.value;
         value1 = num1.value;
-        if (decimals0 != decimals1) revert IncompatibleDecimals(decimals0, decimals1);
-        representation = 10**decimals0;
+        representation = 10**decimals;
         result = value0.mulDiv(representation, value1);
-        return FixedPointValue({value: result, decimals: decimals0});
+        return FixedPointValue({value: result, decimals: decimals});
     }
 
-    function assign(FixedPointValue memory num0, FixedPointValue memory num1) public pure returns (FixedPointValue memory) {
-        uint8 decimals0;
-        uint8 decimals1;
-        uint256 value0;
-        uint256 value1;
-        FixedPointValue memory difference;
-        FixedPointValue memory result;
-        if (decimals0 != decimals1) revert IncompatibleDecimals(decimals0, decimals1);
-        if (value0 > value1) {
-            difference = num0.sub(num1);
-            result = num0.sub(difference);
-            return result;
-        }
-        else if (value0 < value1) {
-            difference = num1.sub(num0);
-            result = num0.add(difference);
-            return result;
-        }
-        else {
-            return num0;
-        }
-    }
-
-    function asEther(FixedPointValue memory num) public pure returns (FixedPointValue memory) {
-        uint8 decimals;
-        uint256 value;
-        uint256 result;
-        decimals = num.decimals;
-        value = num.value;
-        if (value == 0) return FixedPointValue({value: 0, decimals: ether_()});
-        result = ((value * (10**ether_()) / (10**decimals)) * (10**ether_())) / (10**ether_());
-        return FixedPointValue({value: result, decimals: ether_()});
-    }
-
-    function asNewDecimals(FixedPointValue memory num, uint8 decimals) public pure returns (FixedPointValue memory) {
+    function valueWithNewDecimals_(FixedPointValue memory num, uint8 decimals) public pure returns (FixedPointValue memory) {
         uint8 currentDecimals;
         uint256 value;
         uint256 result;
@@ -131,36 +148,26 @@ library FixedPointMath {
         return FixedPointValue({value: result, decimals: currentDecimals});
     }
 
-    function scale(FixedPointValue memory num0, FixedPointValue memory num1) public pure returns (BasisPointValue memory) {
-        uint8 decimals0;
-        uint8 decimals1;
-        uint256 representation;
-        FixedPointValue memory result;
-        FixedPointValue memory scale;
-        decimals0 = num0.decimals;
-        decimals1 = num1.decimals;
-        representation = 10**decimals0;
-        scale = FixedPointValue({value: 10_000 * representation, decimals: decimals0});
-        if (decimals0 != decimals1) revert IncompatibleDecimals(decimals0, decimals1);
-        result = num0.div(num1).mul(scale);
-        return BasisPointValue({value: result});
+    function valueWithEtherDecimals_(FixedPointValue memory num) public pure returns (FixedPointValue memory) {
+        uint8 decimals;
+        uint256 value;
+        uint256 result;
+        decimals = num.decimals;
+        value = num.value;
+        if (value == 0) return FixedPointValue({value: 0, decimals: ether_()});
+        result = ((value * (10**ether_()) / (10**decimals)) * (10**ether_())) / (10**ether_());
+        return FixedPointValue({value: result, decimals: ether_()});
     }
 
-    function slice(FixedPointValue memory num, BasisPointValue memory portion) public pure returns (FixedPointValue memory) {
-        uint8 decimals;
-        uint256 representation;
-        FixedPointValue memory result;
-        FixedPointValue memory scale;
-        FixedPointValue memory portionAsFixedPointValue;
-        decimals = num.decimals;
-        representation = 10**decimals;
-        scale = FixedPointValue({value: 10_000 * representation, decimals: decimals});
-        portionAsFixedPointValue = portion.value;
-        result = num.div(scale).mul(portionAsFixedPointValue);
-        return result;
+    function requireCompatibleDecimals_(FixedPointValue memory num0, FixedPointValue memory num1) private pure {
+        uint8 decimals0;
+        uint8 decimals1;
+        decimals0 = num0.decimals;
+        decimals1 = num1.decimals;
+        if (decimals0 != decimals1) revert IncompatibleDecimals(decimals0, decimals1);
     }
 
     function ether_() private pure returns (uint8) {
-        return Static.ether();
+        return 18;
     }
 }
