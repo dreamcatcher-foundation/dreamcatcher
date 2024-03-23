4b323db607f30d0267ec3020f39fa481ab35d90c
snqre <marco.bizxaro@gmail.com>
2024-03-23 21:12:33 +0000

    Update FixedPointMath.sol

--- code/contract/sol/native/math/FixedPointMath.sol
+++ code/contract/sol/native/math/FixedPointMath.sol
@@ -6,35 +6,39 @@ import { FixedPointValue } from "../shared/FixedPointValue.sol";
 contract FixedPointMath {
     using Math for uint256;
 
-    error FixedPointTypeError(FixedPointValue memory num0, FixedPointValue memory num1);
-    error FixedPointTypeMatchingError(FixedPointValue[] memory nums);
+    error FixedPointTypeError(FixedPointValue num0, FixedPointValue num1);
 
-    modifier onlySimilarFixedPointType(FixedPointValue[] memory nums) {
-        _onlySimilarFixedPointType(nums);
+    modifier only2SimilarFixedPointTypes(FixedPointValue memory num0, FixedPointValue memory num1) {
+        _onlySimilarFixedPointTypes(num0, num1);
         _;
     }
 
-    function _slice(FixedPointValue memory num, FixedPointValue memory sliceAsBasisPoints) internal pure onlySimilarFixedPointType([num, sliceAsBasisPoints]) returns (FixedPointValue memory) {
+    modifier only3SimilarFixedPointTypes(FixedPointValue memory num0, FixedPointValue memory num1, FixedPointValue memory num2) {
+        _onlySimilarFixedPointTypes(num0, num1, num2);
+        _;
+    }
+
+    function _slice(FixedPointValue memory num, FixedPointValue memory sliceAsBasisPoints) internal pure only2SimilarFixedPointTypes(num, sliceAsBasisPoints) returns (FixedPointValue memory) {
         return _mul(_div(num, _fullScale(num.decimals)), sliceAsBasisPoints);
     }
 
-    function _scale(FixedPointValue memory num0, FixedPointValue memory num1) internal pure onlySimilarFixedPointType([num0, num1]) returns (FixedPointValue memory asBasisPoints) {
+    function _scale(FixedPointValue memory num0, FixedPointValue memory num1) internal pure only2SimilarFixedPointTypes(num0, num1) returns (FixedPointValue memory asBasisPoints) {
         return _mul(_div(num0, num1), _fullScale(num0.decimals));
     }
 
-    function _add(FixedPointValue memory num0, FixedPointValue memory num1) internal pure onlySimilarFixedPointType([num0, num1]) returns (FixedPointValue memory) {
+    function _add(FixedPointValue memory num0, FixedPointValue memory num1) internal pure only2SimilarFixedPointTypes(num0, num1) returns (FixedPointValue memory) {
         return FixedPointValue({value: num0.value + num1.value, decimals: num0.decimals});
     }
 
-    function _sub(FixedPointValue memory num0, FixedPointValue memory num1) internal pure onlySimilarFixedPointType([num0, num1]) returns (FixedPointValue memory) {
+    function _sub(FixedPointValue memory num0, FixedPointValue memory num1) internal pure only2SimilarFixedPointTypes(num0, num1) returns (FixedPointValue memory) {
         return FixedPointValue({value: num0.value - num1.value, decimals: num0.decimals});
     }
 
-    function _mul(FixedPointValue memory num0, FixedPointValue memory num1) internal pure onlySimilarFixedPointType([num0, num1]) returns (FixedPointValue memory) {
-        return FixedPointValue({value: _mulDiv(num0.value, num1.value, _representation(num0.decimals), decimals: num0.decimals)});
+    function _mul(FixedPointValue memory num0, FixedPointValue memory num1) internal pure only2SimilarFixedPointTypes(num0, num1) returns (FixedPointValue memory) {
+        return FixedPointValue({value: _mulDiv(num0.value, num1.value, _representation(num0.decimals)), decimals: num0.decimals});
     }
 
-    function _div(FixedPointValue memory num0, FixedPointValue memory num1) internal pure onlySimilarFixedPointType([num0, num1]) returns (FixedPointValue memory) {
+    function _div(FixedPointValue memory num0, FixedPointValue memory num1) internal pure only2SimilarFixedPointTypes(num0, num1) returns (FixedPointValue memory) {
         return FixedPointValue({value: _mulDiv(num0.value, _representation(num0.decimals), num1.value), decimals: num0.decimals});
     }
 
@@ -43,11 +47,7 @@ contract FixedPointMath {
     }
 
     function _asNewDecimals(FixedPointValue memory num, uint8 decimals) internal pure returns (FixedPointValue memory) {
-        uint8 decimals0 = num.decimals;
-        uint8 decimals1 = decimals;
-        uint256 value = num.value;
-        value = _mulDiv(value, _representation(decimals1), _representation(decimals0));
-        return FixedPointValue({value: value, decimals: decimals1});
+        return FixedPointValue({value: _mulDiv(num.value, _representation(decimals), _representation(num.decimals)), decimals: decimals});
     }
 
     function _fullScale(uint8 decimals) private pure returns (FixedPointValue memory asBasisPoints) {
@@ -58,12 +58,13 @@ contract FixedPointMath {
         return 10**decimals;
     }
 
-    function _onlySimilarFixedPointType(FixedPointValue[] memory nums) private pure {
-        _checkMinFixedPointTypeMatches(nums);
-        _checkMaxFixedPointTypeMatches(nums);
-        for (uint8 i = 1; i < nums.length; i++) {
-            _onlyIfFixedPointTypesMatch(nums[0], nums[i]);
-        }
+    function _onlySimilarFixedPointTypes(FixedPointValue memory num0, FixedPointValue memory num1) private pure {
+        _onlyIfFixedPointTypesMatch(num0, num1);
+    }
+
+    function _onlySimilarFixedPointTypes(FixedPointValue memory num0, FixedPointValue memory num1, FixedPointValue memory num2) private pure {
+        _onlyIfFixedPointTypesMatch(num0, num1);
+        _onlyIfFixedPointTypesMatch(num0, num2);
     }
 
     function _onlyIfFixedPointTypesMatch(FixedPointValue memory num0, FixedPointValue memory num1) private pure {
@@ -72,19 +73,7 @@ contract FixedPointMath {
         }
     }
 
-    function _checkMinFixedPointTypeMatches(FixedPointValue[] memory nums) private pure {
-        if (nums.length < 2) {
-            revert FixedPointTypeMatchingError(nums);
-        }
-    }
-
-    function _checkMaxFixedPointTypeMatches(FixedPointValue[] memory nums) private pure {
-        if (nums.length > 9) {
-            revert FixedPointTypeMatchingError(nums);
-        }
-    }
-
     function _mulDiv(uint256 num0, uint256 num1, uint256 num2) private pure returns (uint256) {
-        return Math.mulDiv(num0, num1);
+        return Math.mulDiv(num0, num1, num2);
     }
 }
