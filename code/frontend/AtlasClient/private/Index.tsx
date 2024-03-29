dca148bfe7f362537112160eccfc0a2533619542
snqre <marco.bizxaro@gmail.com>
2024-03-29 22:39:45 +0000

    Update Index.tsx

--- code/frontend/AtlasClient/private/Index.tsx
+++ code/frontend/AtlasClient/private/Index.tsx
@@ -1,6 +1,11 @@
 import {render} from "./Boilerplate.tsx";
 import {createBrowserRouter} from "react-router-dom";
 import {HomePage} from "./page/HomePage.tsx";
+import {includeMetamaskOperator} from "./operators/MetamaskOperator.tsx";
+import {includeClockOperator} from "./operators/ClockOperator.tsx";
+
+includeMetamaskOperator();
+includeClockOperator();
 
 render(createBrowserRouter([{
     path: "/code/frontend/AtlasClient/Index.html",
