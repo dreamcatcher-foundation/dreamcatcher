c24994e05ff5ca148bc2970646a9f65b65fb441f
snqre <marco.bizxaro@gmail.com>
2024-05-08 02:45:52 +0100

    clean up

--- code/build/haruka/app/library/node/Gateway.ts
+++
@@ -1,8 +0,0 @@
-import {defaultMappedEventEmitter} from "../messenger/DefaultMappedEventEmitter.ts";
-
-function bootGateway() {
-    let nodeName: string = "gateway";
-    defaultMappedEventEmitter.hook(nodeName, "query", function() {
-        
-    });
-}
