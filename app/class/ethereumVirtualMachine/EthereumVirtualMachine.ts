17344b602a80519e9f2cad30ed7d67dcc2d8acab
snqre <marco.bizxaro@gmail.com>
2024-06-09 12:45:05 +0100

    Update EthereumVirtualMachine.ts

--- app/class/ethereumVirtualMachine/EthereumVirtualMachine.ts
+++ app/class/ethereumVirtualMachine/EthereumVirtualMachine.ts
@@ -1,2 +1,32 @@
 import { ethers as Ethers } from "ethers";
 import * as TsResult from "ts-results";
+
+function EthereumVirtualMachine({ signer }: { signer: Ethers.Wallet; }) {
+    async function signerAddress(): Promise<string> {
+        return await signer.getAddress();
+    }
+
+    async function generateNonce(): Promise<number> {
+        return await signer.getNonce();
+    }
+
+    async function query({
+        to,
+        methodSignature,
+        methodName,
+        methodArgs=[]}: {
+            to: string;
+            methodSignature: string;
+            methodName: string;
+            methodArgs?: unknown[];
+    }): Promise<TsResult.Result<unknown, unknown>> {
+        try {
+            return TsResult.Ok<unknown>(await new Ethers.Contract(to, [methodSignature], this._signer.provider).getFunction(methodName)(...methodArgs));
+        }
+        catch (error: unknown) {
+            return TsResult.Err<unknown>(error);
+        }
+    }
+
+    
+}
