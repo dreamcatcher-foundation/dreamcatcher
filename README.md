c9559cf809e5cbb6be90345e4c8a9b752758e24f
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2024-08-13 12:00:10 +0200

    complete image component

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 [x] react navbar component.
 [x] react text component.
+[o] react image component.
 [o] react layout components.
 [o] review and refactor code where neccesary.
 [o] complete account for server side.
---
+++ src/public/component/misc/Image.tsx
@@ -0,0 +1,19 @@
+import type { RowProps } from "@component/Row";
+import { Row } from "@component/Row";
+import React from "react";
+
+export interface ImageProps extends RowProps {
+    src: string;
+}
+
+export function Image(props: ImageProps): React.JSX.Element {
+    let { src, style, children, ... more } = props;
+    return <>
+        <Row
+        style={{
+            backgroundImage: `url(${ src })`,
+            backgroundSize: "cover"
+        }}
+        { ... more }/>
+    </>;
+}
