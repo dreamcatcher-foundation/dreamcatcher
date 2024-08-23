5d197e76c195dac8b2e662671ce58ff989436e17
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2024-08-23 13:28:24 +0200

    update erc20 wrapper component

--- src/public/component/module/client/wrapper/Erc20.tsx
+++ src/public/component/module/client/wrapper/Erc20.tsx
@@ -73,7 +73,13 @@ export type Erc20
     };
 
 export function Erc20(_address: string): Erc20 {
-    async function name() {
+    async function name():
+        Promise<
+            | Ok<string>
+            | NotConnectedErr
+            | TypeErr
+            | Err<unknown>
+        > {
         let query:
             | Ok<unknown>
             | NotConnectedErr
@@ -87,4 +93,111 @@ export function Erc20(_address: string): Erc20 {
         if (typeof query.unwrap() !== "string") return Err("invalidType");
         return Ok<string>(query.unwrap() as string);
     }
+    async function symbol():
+        Promise<
+            | Ok<string>
+            | NotConnectedErr
+            | TypeErr
+            | Err<unknown>
+        > {
+        let query:
+            | Ok<unknown>
+            | NotConnectedErr
+            | Err<unknown> 
+            = await Client.query(Query({
+                to: _address,
+                methodSignature: "function symbol() external view returns (string)",
+                methodName: "symbol"
+            }));
+        if (query.err) return query;
+        if (typeof query.unwrap() !== "string") return Err("invalidType");
+        return Ok<string>(query.unwrap() as string);
+    }
+    async function decimals():
+        Promise<
+            | Ok<bigint>
+            | NotConnectedErr
+            | TypeErr
+            | Err<unknown>    
+        > {
+        let query:
+            | Ok<unknown>
+            | NotConnectedErr
+            | Err<unknown>
+            = await Client.query(Query({
+                to: _address,
+                methodSignature: "function decimals() external view returns (uint8)",
+                methodName: "decimals"
+            }));
+        if (query.err) return query;
+        if (typeof query.unwrap() !== "bigint") return Err("invalidType");
+        return Ok<bigint>(query.unwrap() as bigint);
+    }
+    async function totalSupply():
+        Promise<
+            | Ok<number>
+            | NotConnectedErr
+            | TypeErr
+            | Err<unknown>
+        > {
+        let query:
+            | Ok<unknown>
+            | NotConnectedErr
+            | Err<unknown>
+            = await Client.query(Query({
+                to: _address,
+                methodSignature: "function totalSupply() external view returns (uint256)",
+                methodName: "totalSupply"
+            }));
+        if (query.err) return query;
+        if (typeof query.unwrap() !== "bigint") return Err("invalidType");
+        let decimals_:
+            | Ok<bigint>
+            | NotConnectedErr
+            | TypeErr
+            | Err<unknown>
+            = await decimals();
+        if (decimals_.err) return decimals_;
+        let queryAsBigint: bigint = query.unwrap() as bigint;
+        let decimalsAsBigint: bigint = decimals_.unwrap() as bigint;
+        let queryAsNumber: number = Number(queryAsBigint);
+        let decimalsAsNumber: number = Number(decimalsAsBigint);
+        let conversion: number = queryAsNumber / 10**decimalsAsNumber;
+        return Ok<number>(conversion);
+    }
+    async function balanceOf(account: string):
+        Promise<
+            | Ok<number>
+            | NotConnectedErr
+            | TypeErr
+            | Err<unknown>
+        > {
+        let query:
+            | Ok<unknown>
+            | NotConnectedErr
+            | Err<unknown>
+            = await Client.query(Query({
+                to: _address,
+                methodSignature: "function balanceOf(address) external view returns (uint256)",
+                methodName: "balanceOf",
+                methodArgs: [account]
+            }));
+        if (query.err) return query;
+        if (typeof query.unwrap() !== "bigint") return Err("invalidType");
+        let decimals_:
+            | Ok<bigint>
+            | NotConnectedErr
+            | TypeErr
+            | Err<unknown>
+            = await decimals();
+        if (decimals_.err) return decimals_;
+        let queryAsBigint: bigint = query.unwrap() as bigint;
+        let decimalsAsBigint: bigint = decimals_.unwrap() as bigint;
+        let queryAsNumber: number = Number(queryAsBigint);
+        let decimalsAsNumber: number = Number(decimalsAsBigint);
+        let conversion: number = queryAsNumber / 10**decimalsAsNumber;
+        return Ok<number>(conversion);
+    }
+
+    return {name, symbol, decimals, totalSupply, balanceOf};
 }
